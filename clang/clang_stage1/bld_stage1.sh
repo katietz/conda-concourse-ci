52f9c96638f990de022e3bb5e66a6b57bc26410a
Kai Tietz <ktietz@anaconda.com>
2020-05-06 12:25:21 +0200

    Update clang build

--- clang/clang_stage1/bld_stage1.sh
+++ clang/clang_stage1/bld_stage1.sh
@@ -5,13 +5,13 @@ echo "Do initial bootstrap compiler activation ..."
 conda-build -c local -m cbc_boot_from.yaml clang_bootstrap_osx-64-feedstock
 echo "Now we can start to do standard ...dev builds for new version"
 conda-build -c local -m cbc_boot_from.yaml llvmdev-feedstock
-conda-build -c local -m cbc_boot_from.yaml clangdev-feedstock
+conda-build -c local -m cbc_boot_from2.yaml clangdev-feedstock
 echo "First runtime library"
-conda-build -c local -m cbc_boot_from.yaml libcxx-feedstock
-conda-build -c local -m cbc_boot_from.yaml tapi-feedstock
+conda-build -c local -m cbc_boot_from2.yaml libcxx-feedstock
+conda-build -c local -m cbc_boot_from2.yaml tapi-feedstock
 echo "Now we attempt to eat our own food ..."
 conda-build -c local -e ../conda_build_config.yaml cctools-and-ld64-feedstock
 conda-build -c local -e ../conda_build_config.yaml compiler-rt-feedstock
 conda-build -c local -e ../conda_build_config.yaml clang-compiler-activation-feedstock
-echo "Hurry, we reached initial state ... now next stage"
+echo "Hurray, we reached initial state ... now next stage"
 
