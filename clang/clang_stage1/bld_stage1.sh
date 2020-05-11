22c9abe6561a6eb3f412f86e94d219ca992ca300
Kai Tietz <ktietz@anaconda.com>
2020-05-11 16:36:36 +0200

    Update

--- clang/clang_stage1/bld_stage1.sh
+++ clang/clang_stage1/bld_stage1.sh
@@ -2,12 +2,12 @@
 
 echo "Begin to build stage1 of clang"
 echo "Do initial bootstrap compiler activation ..."
-conda-build -c local -m cbc_boot_from.yaml clang_bootstrap_osx-64-feedstock
+conda-build -c local -e ../conda_build_config_4.yaml -m cbc_boot_from.yaml clang_bootstrap_osx-64-feedstock
 echo "Now we can start to do standard ...dev builds for new version"
-conda-build -c local -m cbc_boot_from.yaml llvmdev-feedstock
-conda-build -c local -m cbc_boot_from2.yaml clangdev-feedstock
+conda-build -c local -e ../conda_build_config_4.yaml -m cbc_boot_from.yaml llvmdev-feedstock
+conda-build -c local -e ../conda_build_config_4.yaml -m cbc_boot_from2.yaml clangdev-feedstock
 echo "First runtime library"
-conda-build -c local -m cbc_boot_from2.yaml libcxx-feedstock
+conda-build -c local -e ../conda_build_config_4.yaml -m cbc_boot_from2.yaml libcxx-feedstock
 conda-build -c local -m cbc_boot_from2.yaml tapi-feedstock
 echo "Now we attempt to eat our own food ..."
 conda-build -c local -e ../conda_build_config.yaml cctools-and-ld64-feedstock
